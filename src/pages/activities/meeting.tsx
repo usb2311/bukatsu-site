7b538a52016d7df1673c5de42095e9400769582f
usb2311 <r28i28@gmail.com>
2026-02-14 22:06:17 +0900

    embed meeting video

--- src/pages/activities/meeting.tsx
+++ src/pages/activities/meeting.tsx
@@ -91,7 +91,7 @@ export default function Meeting() {
 
   <div className="aspect-video">
     <iframe
-      src="https://kosenjp.sharepoint.com/:f:/r/sites/msteams_fb6309/Shared%20Documents/General/2025_1%E5%B9%B4%E7%94%9F%E5%90%91%E3%81%91%E8%AC%9B%E5%BA%A7(%E5%85%A83%E5%9B%9E)?csf=1&web=1&e=g5bG5s"
+      src="https://kosenjp.sharepoint.com/:v:/s/msteams_fb6309/IQBnxYGmnjUZQr9xZbKiC8Y9AU2oRfJB8J-mtSIU8gmodeY?e=cTfKXC"
       className="w-full h-full rounded-lg"
       allowFullScreen
     ></iframe>
